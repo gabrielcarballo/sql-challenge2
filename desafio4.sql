aaa6e12212af73edd23c45a16518a385a43945d6
Gabriel Carballo <gabrielfpcarballo@gmail.com>
2023-02-05 18:48:20 -0300

    Requisite 4 done

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+SELECT 
+  A.usuario_nome as usuario,
+  IF(YEAR(MAX(B.data_reproducao)) >= 2021, 'Usuário ativo', 'Usuário inativo') AS 'status_usuario'
+FROM `SpotifyClone`.usuario AS A
+LEFT JOIN `SpotifyClone`.historico AS B
+ON A.usuario_id = B.usuario_id
+GROUP BY A.usuario_nome
+ORDER BY A.usuario_nome;
