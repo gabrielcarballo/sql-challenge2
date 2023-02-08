7d4f80a9a0667c8d7c75859d65f6560cc45921ba
Gabriel Carballo <gabrielfpcarballo@gmail.com>
2023-02-07 21:11:34 -0300

    Requisite 8 done + formatting previous files

--- desafio8.sql
+++ desafio8.sql
@@ -1,10 +1,9 @@
 SELECT
-  A.artista_nome AS artista,
-  B.album_nome AS album
+    A.artista_nome AS artista,
+    B.album_nome AS album
 FROM
-`SpotifyClone`.artistas AS A
-INNER JOIN
-  `SpotifyClone`.album AS B
-ON B.artista_id = A.artista_id
-WHERE A.artista_nome = "Elis Regina"
+    `SpotifyClone`.artistas AS A
+    INNER JOIN `SpotifyClone`.album AS B ON B.artista_id = A.artista_id
+WHERE
+    A.artista_nome = "Elis Regina"
 ORDER BY album
