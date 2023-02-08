98552cfacd8ad07c38d72078fa6e2c53bfba7583
Gabriel Carballo <gabrielfpcarballo@gmail.com>
2023-02-08 16:09:52 -0300

    Actually we dont need alias on table in req 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,14 +1,14 @@
 SELECT
-  A.cancao_nome AS nome_musica,
+  cancao_nome AS nome_musica,
 CASE
-  WHEN A.cancao_nome LIKE "%Bard%" THEN REPLACE (A.cancao_nome, "Bard", "QA")
-  WHEN A.cancao_nome LIKE "%Amar%" THEN REPLACE (A.cancao_nome, "Amar", "Code Review")
-  WHEN A.cancao_nome LIKE "%Pais" THEN REPLACE (A.cancao_nome, "Pais", "Pull Requests")
-  WHEN A.cancao_nome LIKE "%SOUL" THEN REPLACE (A.cancao_nome, "SOUL", "CODE")
-  WHEN A.cancao_nome LIKE "%SUPERSTAR" THEN REPLACE (A.cancao_nome, "SUPERSTAR", "SUPERDEV")
+  WHEN cancao_nome LIKE "%Bard%" THEN REPLACE (cancao_nome, "Bard", "QA")
+  WHEN cancao_nome LIKE "%Amar%" THEN REPLACE (cancao_nome, "Amar", "Code Review")
+  WHEN cancao_nome LIKE "%Pais" THEN REPLACE (cancao_nome, "Pais", "Pull Requests")
+  WHEN cancao_nome LIKE "%SOUL" THEN REPLACE (cancao_nome, "SOUL", "CODE")
+  WHEN cancao_nome LIKE "%SUPERSTAR" THEN REPLACE (cancao_nome, "SUPERSTAR", "SUPERDEV")
   END novo_nome
 FROM
-`SpotifyClone`.cancoes AS A
+`SpotifyClone`.cancoes
 GROUP BY nome_musica
 HAVING novo_nome <> 'null'
 ORDER BY nome_musica DESC
