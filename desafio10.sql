68199c990581de352830c1eca5471cb2996f705d
Gabriel Carballo <gabrielfpcarballo@gmail.com>
2023-02-08 15:20:11 -0300

    Test are faling with the exact same expect

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+SELECT
+    A.cancao_nome AS nome,
+    COUNT(B.historico_reproducao) AS reproducoes
+FROM
+    `SpotifyClone`.cancoes AS A
+    INNER JOIN `SpotifyClone`.historico AS B
+    INNER JOIN `SpotifyClone`.usuario AS C ON A.cancao_id = B.historico_reproducao
+    AND C.usuario_id = B.usuario_id
+WHERE
+    C.plano_id = 1
+    OR C.plano_id = 4
+GROUP BY nome
+ORDER BY nome;
